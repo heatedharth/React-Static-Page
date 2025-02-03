122a4f5ce79ef427b1e2ebc524a7c783188efd7b
Amy Nguyen <mail2avn@icloud.com>
2025-02-03 14:58:52 -0500

    First commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# React-Static-Page
---
+++ react-static-page/src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+    return <footer><p>Moral: Slow and steady wins the race.</p></footer>;
+}
+
+export default Footer;
